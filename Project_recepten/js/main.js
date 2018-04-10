7faeffd2061b35a284f9f6ad34111eeb78cd970e
dionnejung <38243081+dionnejung@users.noreply.github.com>
2018-04-10 10:29:07 +0200

    Add files via upload

---
+++ Project_recepten/js/main.js
@@ -0,0 +1,130 @@
+gram = ' gram';
+liter = ' liter';
+minuten = 'minuten';
+persoon_enkelvoud = 'persoon';
+persoon_meervoud = 'personen';
+
+
+Bananen pancakes = {
+	'ingredienten': [
+		'Banaan',
+		'Eieren',
+		'Maple Syrup'
+	], 
+	'hoeveelheden': [
+		'1',
+		'2',
+		'2 eetlepels'
+	], 
+	'Moeilijkheidsgraad':
+		25
+	,
+	'bereidingstijd':
+		15 + minuten
+	,
+	'personen':
+		1 + persoon_enkelvoud
+	,
+	'soort':
+		'ontbijt'
+	,
+	'bereidingswijze':
+		'Prak de banaan en mix hem met de twee eieren. Regel een koekenpan met een goede antiaanbaklaag, verwarm wat olie, doe een beetje beslag in de pan en bak de pannenkoekjes om en om bruin. Giet de maple syrup er overheen als ze nog warm zijn en smullen maar!'
+	;	
+}
+
+Pasta Tonno = {
+	'ingredienten': [
+		'Penne pasta',
+		'groene pesto',
+		'tonijn',
+		'tomaat'
+	], 
+	'hoeveelheden': [
+		'60' + gram,
+		'30' + gram,
+		'100' + gram,
+		'1'
+	], 
+	'Moeilijkheidsgraad':
+		25
+	,
+	'bereidingstijd':
+		15 + minuten
+	,
+	'personen':
+		1 + persoon_enkelvoud
+	,
+	'soort':
+		'diner'
+	,
+	'bereidingswijze':
+		'Kook de pasta volgens bereidingswijze (al dente). Lek de tonijn uit. Snijd tomaten in stukjes. Wanneer de pasta gekookt is, spoel deze af. Meng vervolgens alle ingrediënten met elkaar.'
+	;	
+}
+
+Pastaschelpen met ricotta en artisjok = {
+	'ingredienten': [
+		'pastaschelpen',
+		'ricotta',
+		'spinazie',
+		'artisjokharten',
+		'parmezaanze kaas'
+	], 
+	'hoeveelheden': [
+		'200' + gram,
+		'250' + gram,
+		'200' + gram,
+		'75' + gram,
+		'2 eetlepels'
+	], 
+	'Moeilijkheidsgraad':
+		50
+	,
+	'bereidingstijd':
+		30 + minuten
+	,
+	'personen':
+		2 + persoon_meervoud
+	,
+	'soort':
+		'diner'
+	,
+	'bereidingswijze':
+		'Verwarm de oven voor op 180 graden en bereid de pastaschelpen volgens de aanwijzingen op de verpakking. Giet vervolgens de artisjokken af en snijd ze fijn. Zet een pan met wat olie op het vuur en bak hierin de spinazie tot deze compleet geslonken is. Voeg hierna de gesneden artisjokken, de ricotta en één eetlepel parmezaanse kaas toe. Roer alles goed door elkaar en vul de pastaschelpen met het mengsel. Leg de schelpen 1 voor 1 in een ingevette ovenschaal en strooi tot slot de overige parmezaanse kaas nog over de schelpen heen. Bak de schelpen zo\'n 15 minuten tot ze klaar zijn. '
+	;	
+}
+
+Pavlova = {
+	'ingredienten': [
+		'eiwitten',
+		'citroen',
+		'poedersuiker',
+		'zout',
+	], 
+	'hoeveelheden': [
+		'3',
+		'1',
+		'150' + gram,
+		'1 theelepel'
+	], 
+	'Moeilijkheidsgraad':
+		50
+	,
+	'bereidingstijd':
+		100 + minuten
+	,
+	'personen':
+		4 + persoon_meervoud
+	,
+	'soort':
+		'dessert'
+	,
+	'bereidingswijze':
+		'Verwarm de oven voor op 180 graden en bereid de pastaschelpen volgens de aanwijzingen op de verpakking. Giet vervolgens de artisjokken af en snijd ze fijn. Zet een pan met wat olie op het vuur en bak hierin de spinazie tot deze compleet geslonken is. Voeg hierna de gesneden artisjokken, de ricotta en één eetlepel parmezaanse kaas toe. Roer alles goed door elkaar en vul de pastaschelpen met het mengsel. Leg de schelpen 1 voor 1 in een ingevette ovenschaal en strooi tot slot de overige parmezaanse kaas nog over de schelpen heen. Bak de schelpen zo\'n 15 minuten tot ze klaar zijn. '
+	;	
+}
+
+
+	Hero tutorial angular !!
+
