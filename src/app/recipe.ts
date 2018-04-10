18656d1dbe47739fe52aa64c3f272fbc2bfde5f4
dionnejung <38243081+dionnejung@users.noreply.github.com>
2018-04-10 16:23:03 +0200

    Add files via upload

---
+++ src/app/recipe.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+  //name: string;
+  ingredienten: String[];
+  hoeveelheden: string[];
+  moeilijkheidsgraad: number;
+  bereidingstijd : String;
+  personen: number;
+  soort: String;
+  bereidingswijze: String;
+  // id: number;
+  // img: string;
+}
